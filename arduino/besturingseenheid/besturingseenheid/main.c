8642eabe44d750a5a854585b50ea2b969a11be55
DouwePieterReitsma <d.p.reitsma@gmail.com>
2019-10-30 14:47:40 +0100

    project aangemaakt

---
+++ arduino/besturingseenheid/besturingseenheid/main.c
@@ -0,0 +1,18 @@
+/*
+ * besturingseenheid.c
+ *
+ * Created: 30-10-2019 14:46:02
+ * Author : dprei
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
